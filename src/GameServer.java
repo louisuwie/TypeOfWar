380dcd807185bc21d5220ffb08c8d85672228318
louisuwie_ <115404931+louisuwie@users.noreply.github.com>
2023-04-25 14:09:59 +0800

    Added main file details: Author, Version, and GameServer doc.

--- src/GameServer.java
+++ src/GameServer.java
@@ -1,3 +1,26 @@
+/**
+ @author Louis G. Binwag III (200747) & Maria Charmane Rose E. Naciongayo (214152)
+ @version April 25, 2023
+ **/
+
+/*
+	I have not discussed the Java language code in my program
+	with anyone other than my instructor or the teaching assistants
+	assigned to this course.
+
+	I have not used Java language code obtained from another student,
+	or any other unauthorized source, either modified or unmodified.
+
+	If any Java language code or documentation used in my program
+	was obtained from another source, such as a textbook or website,
+	that has been clearly noted with a proper citation in the comments
+	of my program.
+*/
+
+/*
+    GameServer.java instantiates the server and starts the game.
+*/
+
 public class GameServer {
     public static void main(String[] args) {
         System.out.println("Hello world!");
