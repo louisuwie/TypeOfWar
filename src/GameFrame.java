5a066b991da4ec8eb91a0da223caabd8dd9f4f0c
louisuwie_ <115404931+louisuwie@users.noreply.github.com>
2023-04-25 14:45:46 +0800

    Added and fixed authorship details including class descriptions.

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,3 +1,27 @@
+/**
+ @author Louis G. Binwag III (200747) & Maria Charmane Rose E. Naciongayo (214152)
+ @version April 25, 2023
+ **/
+
+/*
+	I have not discussed the Java language code in my program
+	with anyone other than my instructor or the teaching assistants
+	assigned to this course.
+
+	I have not used Java language code obtained from another student,
+	or any other unauthorized source, either modified or unmodified.
+
+	If any Java language code or documentation used in my program
+	was obtained from another source, such as a textbook or website,
+	that has been clearly noted with a proper citation in the comments
+	of my program.
+*/
+
+/*
+    GameFrame.java handles all the features that happen within frame.
+*/
+
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -11,18 +35,24 @@ public class GameFrame {
         /*
         INITIALISING OF VARIABLES
          */
-        JFrame gameUI = new JFrame("Type Of War");
+        JFrame gameUI = new JFrame("T.o.W");
 
-        JLabel title = new JLabel("Are you ready, to Type for War?");
+        JLabel title = new JLabel("Are you ready to Type for War?");
 
         JButton start = new JButton("Start?");
 
         /*
-        ADDING DETAILS TO VARIABLES
+        SPECIFIC DETAILING OF THE VARIABLES
          */
         gameUI.setSize(1920,1080);
         gameUI.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        gameUI.setLayout(new FlowLayout());
 
+        /*
+        ADDING FEATURES INTO FRAME
+         */
+        gameUI.add(title);
+        gameUI.add(start);
 
         /*
         SET VISIBLE
