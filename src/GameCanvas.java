b1b3591e51a900b5adeaecc43cdb1d96f5af04e7
CharmaneNaciongayo <charmane.naciongayo@gmail.com>
2023-05-12 21:32:49 +0800

    Help me God

--- src/GameCanvas.java
+++ src/GameCanvas.java
@@ -5,16 +5,17 @@ import java.awt.event.*;
 public class GameCanvas extends JComponent {
 
 	RopeAssembly ra;
+	Image bg, op;
     
     public GameCanvas() {
+		op = new ImageIcon("DesignAssets/Opening.png").getImage();
+		bg = new ImageIcon("DesignAssets/Background.png").getImage();
         setFocusable(true);
         setPreferredSize(new Dimension(500,500));
     }
 
     protected void paintComponent(Graphics g) {
-
-		// Cast g into g2d
-		Graphics2D g2d = (Graphics2D) g;
+		Graphics2D g2d = (Graphics2D) g; // Cast g into g2d
 		ra = new RopeAssembly();
         ra.draw(g2d);
     }
